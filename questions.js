467094ca8cf79f279a634454256ff32a7e6b3db9
Liz Wade <11866103+lizwade@users.noreply.github.com>
2024-12-03 13:26:28 +0000

    Add first question

--- questions.js
+++ questions.js
@@ -1,8 +1,21 @@
 // questions.js
 
 const questions = [
-  // Add your questions here
+  
 ];
 
 // Don't worry about this, we're just exporting the questions
 module.exports = questions;
+
+
+
+let question1 = {
+    question_text: "In which Disney room do you come up with ideas?",
+    question_type: "multiple_choice",
+    options: ["Swimming Room", "Critic Room", "Realist Room", "Dreamer Room"],
+    correct_option: 3,
+    correct_explanation: `In the Dreamer room there are no wrong answers so you can come up with lots of ideas.
+                           The Realist room is where you narrow them down to what you can realistically do.
+                           The Critic room is where you look at it from all angles and pick holes in ideas.
+                           The Swimming Room is a pool, probably.`,
+}// Add your questions here
